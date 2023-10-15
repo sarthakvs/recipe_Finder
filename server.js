a16a3fc689b358029338ddb144a84688834613c5
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2023-10-15 22:13:36 +0530

    added better css

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
 mongoose.connect('mongodb://0.0.0.0:27017/recipe-finder', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
